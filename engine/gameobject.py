3553c503ce2211c6b127c51403ec151fc4f5b235
Michał Tuśnio <m.tusnio@gmail.com>
2015-07-31 19:52:31 +0200

    Gameobject's components structure simplified.

--- engine/gameobject.py
+++ engine/gameobject.py
@@ -1,27 +1,15 @@
-import collections
-
-
 class GameObject(object):
     def __init__(self):
-        self.components = collections.OrderedDict()
+        self.components = set()
         self.scene = None
 
-    def addcomponent(self, component):
-        """
-        :param engine.components.basecomponent.BaseComponent component: Component to add
-        """
-
-
-    def removecomponent(self, component):
-        pass
-
-    def run_preframe(self):
+    def update(self):
         """
         Runs object simulation before rendering
         """
         pass
 
-    def run_postframe(self):
+    def update_postframe(self):
         """
         Runs object simulation after rendering
         """

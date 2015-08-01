72be2488acc7992a2995fe9fac3c826da6a940e1
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-01 17:54:02 +0200

    Image is set now to None if it doesn't get loaded

--- engine/components/spriterenderer.py
+++ engine/components/spriterenderer.py
@@ -1,11 +1,13 @@
 import pygame
 import basecomponent
 
+
 class SpriteRenderer(basecomponent.BaseComponent):
     def __init__(self, **kwargs):
         super(self.__class__, self).__init__(**kwargs)
         if kwargs.has_key("path"):
             self.image = pygame.image.load(kwargs["path"])
-
+        else:
+            self.image = None
 
 

65a4319adaed96cc1767a04bb001109c1795536b
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-01 18:40:55 +0200

    Default parameters for transforms

--- engine/components/transform.py
+++ engine/components/transform.py
@@ -4,4 +4,6 @@ import basecomponent
 class Transform(basecomponent.BaseComponent):
     def __init__(self, **kwargs):
         super(self.__class__, self).__init__(**kwargs)
-
+        self.position = kwargs.get("position", (0, 0))
+        self.rotation = kwargs.get("rotation", 0)
+        self.scale = kwargs.get("scale", 1)

950bc91f722b9d4f3304c57a2785e3480606c94f
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-03 02:46:51 +0200

    Tweaks to the remove/add objects

--- engine/scene.py
+++ engine/scene.py
@@ -4,20 +4,22 @@ import collections
 class Scene(object):
     def __init__(self):
         self.objects = collections.OrderedDict()
-        self.maxIndex = 0
         self.camera = None
+        self._maxIndex = 0
 
     def addobject(self, obj):
         """
+        Adds a new object to the scene and changes its scene & id variables
         :param engine.gameobject.GameObject obj: Game object to add to the scene
         """
-        self.objects[self.maxIndex] = obj
-        obj.id = self.maxIndex
-        self.maxIndex += 1
+        self.objects[self._maxIndex] = obj
+        obj.id = self._maxIndex
+        self._maxIndex += 1
         obj.scene = self
 
     def removeobject(self, obj):
         """
+        Removes an object from the scene, reverts the scene & id variables to None
         :param engine.gameobject.GameObject obj: Game object to remove from the scene
         """
         if obj.id is None or not self.objects.has_key(obj.id) or self.objects[obj.id] != obj:
@@ -25,6 +27,7 @@ class Scene(object):
 
         del self.objects[obj.id]
         obj.scene = None
+        obj.id = None
 
     def simulate_preframe(self, dt):
         """

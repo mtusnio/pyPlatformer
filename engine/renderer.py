3c685fd52fe57d4c6cd3dc5eb7cee37bac0ef8c6
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-03 02:07:31 +0200

    Support for cameras added to the renderer via scene.camera

--- engine/renderer.py
+++ engine/renderer.py
@@ -1,6 +1,8 @@
 import pygame
 import components
 import copy
+from engine import math
+
 
 class Renderer(object):
     def __init__(self, screen):
@@ -13,16 +15,24 @@ class Renderer(object):
         """
         self.screen.fill((0, 0, 0))
 
+        cameraposition = (math.Vector2(0, 0), 0)
+        if scene.camera is not None:
+            camera = scene.camera.getcomponent(components.Camera)
+            transform = scene.camera.getcomponent(components.Transform)
+            if transform is not None:
+                cameraposition = (transform.position, transform.rotation)
+
         for obj in scene.objects.values():
             sprites = obj.getcomponents(components.SpriteRenderer)
             for spr in sprites:
                 if spr.image is not None:
-                    self._rendersprite(spr)
+                    self._rendersprite(spr, cameraposition)
 
-    def _rendersprite(self, sprite):
+    def _rendersprite(self, sprite, cameraposition):
         objtransform = sprite.gameobject.getcomponent(components.Transform)
         if objtransform is None:
             self.screen.blit(sprite.image, sprite.image.get_rect())
         else:
-            surface = pygame.transform.rotozoom(copy.copy(sprite.image), objtransform.rotation, objtransform.scale)
-            self.screen.blit(surface, surface.get_rect(center=objtransform.position))
+            surface = pygame.transform.rotozoom(copy.copy(sprite.image), objtransform.rotation - cameraposition[1], objtransform.scale)
+            position = objtransform.position - cameraposition[0]
+            self.screen.blit(surface, surface.get_rect(center=position))

e1b0b4917f8286e1cf53a85d820c6dc3f9076f1d
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-05 16:35:38 +0200

    Renderer name fixes

--- engine/renderer.py
+++ engine/renderer.py
@@ -15,24 +15,24 @@ class Renderer(object):
         """
         self.screen.fill((0, 0, 0))
 
-        cameraposition = (math.Vector2(0, 0), 0)
+        camera_position = (math.Vector2(0, 0), 0)
         if scene.camera is not None:
             camera = scene.camera.get_component(components.Camera)
             transform = scene.camera.get_component(components.Transform)
             if transform is not None:
-                cameraposition = (transform.position, transform.rotation)
+                camera_position = (transform.position, transform.rotation)
 
         for obj in scene.objects.values():
             sprites = obj.get_components(components.SpriteRenderer)
             for spr in sprites:
                 if spr.image is not None:
-                    self._render_sprite(spr, cameraposition)
+                    self._render_sprite(spr, camera_position)
 
-    def _render_sprite(self, sprite, cameraposition):
-        objtransform = sprite.gameobject.get_component(components.Transform)
-        if objtransform is None:
+    def _render_sprite(self, sprite, camera_position):
+        obj_transform = sprite.game_object.get_component(components.Transform)
+        if obj_transform is None:
             self.screen.blit(sprite.image, sprite.image.get_rect())
         else:
-            surface = pygame.transform.rotozoom(copy.copy(sprite.image), objtransform.rotation - cameraposition[1], objtransform.scale)
-            position = objtransform.position - cameraposition[0]
+            surface = pygame.transform.rotozoom(copy.copy(sprite.image), obj_transform.rotation - camera_position[1], obj_transform.scale)
+            position = obj_transform.position - camera_position[0]
             self.screen.blit(surface, surface.get_rect(center=position))

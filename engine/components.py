40329f9f59b55a6c871f6785eaae2e35dac1d74c
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-11 03:01:43 +0200

    Missing imports, type hinting

--- engine/components.py
+++ engine/components.py
@@ -1,9 +1,12 @@
 __author__ = 'Maverick'
 import pygame
 from engine import math
-
+import tiledmap
 
 class BaseComponent(object):
+    """
+    :type game_object: engine.GameObject
+    """
     def __init__(self, **kwargs):
         self.game_object = None
 
@@ -15,27 +18,43 @@ class BaseComponent(object):
 
 
 class Camera(BaseComponent):
+    """
+    :type fov: float
+    """
     def __init__(self, **kwargs):
         super(Camera, self).__init__(**kwargs)
         self.fov = kwargs.get("fov", 80)
 
 
 class Renderable(BaseComponent):
+    """
+    :type should_render: bool
+    """
     def __init__(self, **kwargs):
         super(Renderable, self).__init__(**kwargs)
         self.should_render = True
 
 
 class SpriteRenderer(Renderable):
+    """
+    :type image_path: basestring
+    :type image: pygame.image
+    """
     def __init__(self, **kwargs):
         super(SpriteRenderer, self).__init__(**kwargs)
-        if kwargs.has_key("path"):
-            self.image = pygame.image.load(kwargs["path"])
+        self.image_path = kwargs.get("path", None)
+        if self.image_path is not None:
+            self.image = pygame.image.load(self.image_path)
         else:
             self.image = None
 
 
 class Transform(BaseComponent):
+    """
+    :type position: math.Vector2
+    :type rotation: float
+    :type scale: float
+    """
     def __init__(self, **kwargs):
         super(Transform, self).__init__(**kwargs)
         self.position = kwargs.get("position", math.Vector2(0, 0))
@@ -44,6 +63,10 @@ class Transform(BaseComponent):
 
 
 class TiledMap(Renderable):
+    """
+    :type map_path: basestring
+    :type map: tiledmap.TiledMap
+    """
     def __init(self, **kwargs):
         super(TiledMap, self).__init__(**kwargs)
         self.map_path = kwargs.get("map_path", None)
--- engine/tiledmap.py
+++ engine/tiledmap.py
@@ -1,3 +1,4 @@
 __author__ = 'Maverick'
 
 from pytmx.util_pygame import load_pygame as load
+from pytmx import TiledMap

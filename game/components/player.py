b31a3e005a64cc8c0c627115b8df815e9896ac7a
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-15 15:37:21 +0200

    Player object class

--- game/components/player.py
+++ game/components/player.py
@@ -3,10 +3,29 @@ from engine import components
 
 
 class Player(components.BaseComponent):
+    GRAVITY = 320.0
+
     def __init__(self, **kwargs):
         super(Player, self).__init__(**kwargs)
+        self.flying = True
 
     def update(self):
         player = self.game_object
+
         camera = player.scene.camera
         camera.transform.position = player.transform.position
+
+        tiled_map = player.scene.get_object_of_type(components.TiledMap)
+        if tiled_map is None:
+            return
+
+        transform = player.transform
+        pos = transform.position
+        if not tiled_map.is_position_in_map(pos):
+            return
+        x, y = tiled_map.get_tile_for_position(pos)
+
+        dt = self.game_object.scene.dt
+        #if self.flying:
+        #    transform.position[1] += self.GRAVITY * dt
+
--- game/platformer.py
+++ game/platformer.py
@@ -15,17 +15,17 @@ class Platformer(Application):
         self.scene = Scene(self)
 
         map_obj = GameObject()
+        self.scene.add_object(map_obj)
         map_obj.add_components(engine.components.Transform(),
                                engine.components.TiledMap(path="assets\levels\level01.tmx"))
-        self.scene.add_object(map_obj)
 
         self.scene.camera = GameObject()
-        self.scene.camera.add_components(engine.components.Transform(), engine.components.Camera())
         self.scene.add_object(self.scene.camera)
+        self.scene.camera.add_components(engine.components.Transform(), engine.components.Camera())
 
         Input.bindings["forward"] = pygame.K_w
         Input.bindings["left"] = pygame.K_a
         Input.bindings["back"] = pygame.K_s
         Input.bindings["right"] = pygame.K_d
 
-        tiledmap.fill_scene_with_objects(self.scene, map_obj.get_component(engine.components.TiledMap).map)
+        map_obj.get_component(engine.components.TiledMap).fill_scene_with_objects()

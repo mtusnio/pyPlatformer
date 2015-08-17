b579006880e34de17b7710628e7aae905161415d
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-17 02:23:26 +0200

    Velocity based movement

--- game/components/player.py
+++ game/components/player.py
@@ -1,15 +1,22 @@
 __author__ = 'Maverick'
 from engine import components, Input
 from engine.input import BindingDoesNotExist
+from engine.math import Vector2
 import logging
 
 
 class Player(components.BaseComponent):
-    GRAVITY = 320.0
+    GRAVITY_PER_SECOND = 825
+    MOVEMENT_PER_SECOND = 1700
+    JUMP_SPEED = 500
+
+    MAX_VERTICAL_SPEED = 1200
+    MAX_HORIZONTAL_SPEED = 500
 
     def __init__(self, **kwargs):
         super(Player, self).__init__(**kwargs)
         self.flying = True
+        self.velocity = Vector2(0, 0)
 
     def update(self):
         player = self.game_object
@@ -28,10 +35,22 @@ class Player(components.BaseComponent):
 
         dt = self.game_object.scene.dt
         try:
+            binding_pressed = False
             if Input.is_binding_pressed("left"):
-                transform.position[0] -= 400 * dt
+                binding_pressed = True
+                self.velocity[0] -= self.MOVEMENT_PER_SECOND * dt
             if Input.is_binding_pressed("right"):
-                transform.position[0] += 400 * dt
+                binding_pressed = True
+                self.velocity[0] += self.MOVEMENT_PER_SECOND * dt
+
+            if not binding_pressed:
+                sign = (1, -1)[self.velocity[0] < 0]
+                self.velocity[0] -= sign * min(self.MOVEMENT_PER_SECOND * dt, abs(self.velocity[0]))
+
+            if Input.is_binding_pressed("jump"):
+                if not self.flying:
+                    self.velocity[1] = -self.JUMP_SPEED
+
         except BindingDoesNotExist as e:
             logging.warning("Binding not found: " + str(e))
 
@@ -41,7 +60,13 @@ class Player(components.BaseComponent):
                 self.flying = True
 
         if self.flying:
-            transform.position[1] += self.GRAVITY * dt
+            self.velocity[1] += self.GRAVITY_PER_SECOND * dt
+
+        # Velocity clamping
+        self.velocity[0] = sorted((-self.MAX_HORIZONTAL_SPEED, self.velocity[0], self.MAX_HORIZONTAL_SPEED))[1]
+        self.velocity[1] = sorted((-self.MAX_VERTICAL_SPEED, self.velocity[1], self.MAX_VERTICAL_SPEED))[1]
+
+        transform.position = position + self.velocity * dt
 
     def collide(self, game_object, *rectangles):
         bounding_rectangle = self.game_object.get_component(components.BoundingRectangle).rectangle
@@ -56,3 +81,4 @@ class Player(components.BaseComponent):
                 closest_lower = min(lower_rectangles, key=lambda rect: (position - rect.center).length())
                 transform.position.y = closest_lower.top - bounding_rectangle.height/2
                 self.flying = False
+                self.velocity[1] = 0

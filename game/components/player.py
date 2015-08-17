33311670d05c0b42727cb3e074d4cb89a33522fc
Michał Tuśnio <m.tusnio@gmail.com>
2015-08-17 22:31:21 +0200

    Simplified player controller

--- game/components/player.py
+++ game/components/player.py
@@ -8,7 +8,7 @@ import logging
 class Player(components.BaseComponent):
     GRAVITY_PER_SECOND = 825
     MOVEMENT_PER_SECOND = 1700
-    JUMP_SPEED = 500
+    JUMP_SPEED = 700
 
     MAX_VERTICAL_SPEED = 1200
     MAX_HORIZONTAL_SPEED = 500
@@ -17,9 +17,9 @@ class Player(components.BaseComponent):
         super(Player, self).__init__(**kwargs)
         self.flying = True
         self.velocity = Vector2(0, 0)
-        self._applied_velocity = Vector2(0, 0)
+        self.applied_velocity = Vector2(0, 0)
 
-    def update(self):
+    def update_postframe(self):
         player = self.game_object
 
         camera = player.scene.camera
@@ -31,6 +31,8 @@ class Player(components.BaseComponent):
 
         transform = player.transform
         position = transform.position
+        bounding_rectangle = self.game_object.get_component(components.BoundingRectangle).rectangle
+
         if not tiled_map.is_position_in_map(position):
             return
 
@@ -57,8 +59,15 @@ class Player(components.BaseComponent):
             logging.warning("Binding not found: " + str(e))
 
         if not self.flying:
-            x, y = tiled_map.get_tile_for_position(position)
-            if not tiled_map.get_tile_properties(x, y + 1)["collidable"]:
+            test_rectangle = bounding_rectangle.move(Vector2(0, 3))
+            map_tiles = tiled_map.get_tiles_for_area(test_rectangle)
+
+            found_collidable = False
+            for tile in map_tiles:
+                if tiled_map.get_tile_properties(tile[0], tile[1])["collidable"]:
+                    found_collidable = True
+                    break
+            if not found_collidable:
                 self.flying = True
 
         if self.flying:
@@ -68,8 +77,8 @@ class Player(components.BaseComponent):
         self.velocity.x = sorted((-self.MAX_HORIZONTAL_SPEED, self.velocity.x, self.MAX_HORIZONTAL_SPEED))[1]
         self.velocity.y = sorted((-self.MAX_VERTICAL_SPEED, self.velocity.y, self.MAX_VERTICAL_SPEED))[1]
 
-        self._applied_velocity = self.velocity * dt
-        transform.position = position + self._applied_velocity
+        self.applied_velocity = self.velocity * dt
+        transform.position = position + self.applied_velocity
 
     def collide(self, game_object, *rectangles):
         bounding_rectangle = self.game_object.get_component(components.BoundingRectangle).rectangle
@@ -77,42 +86,23 @@ class Player(components.BaseComponent):
         position = transform.position
 
         if game_object.has_component(components.TiledMapCollider):
-            remaining_rectangles = list(rectangles)
-            prev_velocity = self._applied_velocity
-            prev_position = bounding_rectangle.move(-prev_velocity)
-
-            horizontal_rectangle = prev_position.move(Vector2(prev_velocity.x, 0))
-            horizontal_collision = horizontal_rectangle.collidelistall(remaining_rectangles)
-            if len(horizontal_collision) > 0:
-                rectangle = remaining_rectangles[horizontal_collision[0]]
-                if position.x < rectangle.centerx:
-                    position.x = rectangle.left - bounding_rectangle.width/2 - 2
-                else:
-                    position.x = rectangle.right + bounding_rectangle.width/2 + 2
-                self.velocity.x = 0
-                remaining_rectangles = [x for y,x in enumerate(remaining_rectangles) if y not in horizontal_collision]
-
-            upper_rectangles = [rect for rect in remaining_rectangles if bounding_rectangle.top < rect.bottom
-                                    and bounding_rectangle.bottom > rect.bottom]
-            lower_rectangles = [rect for rect in remaining_rectangles if bounding_rectangle.bottom > rect.top
-                                    and bounding_rectangle.top < rect.top]
-
-            if self.flying:
-                vertical_rectangle = prev_position.move(Vector2(0, prev_velocity.y))
-
-                lower_collisions = vertical_rectangle.collidelistall(lower_rectangles)
-                if len(lower_collisions) > 0:
-                    rectangle = lower_rectangles[lower_collisions[0]]
-                    position.y = rectangle.top - bounding_rectangle.height/2 - 2
-                    self.flying = False
+            prev_rectangle = bounding_rectangle.move(-self.applied_velocity)
+            #horizontal_rectangle = bounding_rectangle.move(Vector2(self._applied_velocity.x, 0))
+            #vertical_rectangle = bounding_rectangle.move(Vector2(0, self._applied_velocity.y))
+            epsilon = 0.5
+            for collided in rectangles:
+                if prev_rectangle.bottom - collided.top <= epsilon:
                     self.velocity.y = 0
-
-                upper_collisions = vertical_rectangle.collidelistall(upper_rectangles)
-                if len(upper_collisions) > 0:
-                    rectangle = upper_rectangles[upper_collisions[0]]
-                    position.y = rectangle.bottom + bounding_rectangle.height/2 + 2
+                    self.flying = False
+                    position.y = collided.top - bounding_rectangle.height/2 - 1
+                    continue
+                elif prev_rectangle.top - collided.bottom >= epsilon:
                     self.velocity.y = abs(self.velocity.y)
+                    position.y = collided.bottom + bounding_rectangle.height/2 + 1
+                    continue
 
-                remaining_rectangles = [x for y, x in enumerate(rectangles) if y not in lower_collisions
-                                        and y not in upper_collisions]
-
+                self.velocity.x = 0
+                if position.x < collided.centerx:
+                    position.x = collided.left - bounding_rectangle.width/2 - 1
+                else:
+                    position.x = collided.right + bounding_rectangle.width/2 + 1
